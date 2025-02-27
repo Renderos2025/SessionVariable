9210a45f962cde94dc932f96c81b6f918a64a1ff
Carlos Renderos <carlos.renderos.cats2020gmail.com>
2025-02-26 21:31:00 -0600

    Primer commit

---
+++ Session_variable4367416/SessionVariable.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Session_variable4367416
+{
+    public partial class SessionVariable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Session["ddlCategory"] = ddlCategory.SelectedValue;
+            Session["ddlSupplier"] = ddlSupplier.SelectedValue;
+            Session["strProduct"] = txtProduct.Text;
+            Session["strDescription"] = TextArea1.InnerText;
+            Session["strImage"] = txtImage.Text;
+            Session["decPrice"] = txtPrice.Text;
+            Session["bytNumberInStock"] = txtNumberInStock.Text;
+            Session["bytNumberOnOrder"] = txtNumberOnOrder.Text;
+            Session["bytReorderLevel"] = txtReorderLevel.Text;
+
+            Response.Redirect("ProductConfirmSessionVariable.aspx");
+        }
+    }
+}
